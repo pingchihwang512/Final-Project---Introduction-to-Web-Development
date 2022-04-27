75a20cc875d38c865e19838f57e57b3daf8e9e77
Ping <pingchihwang512@gmail.com>
2022-04-27 07:27:02 +0000

    update for highlight the number
    Signed-off-by: Ping <pingchihwang512@gmail.com>

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ function compute()
         var year = new Date().getFullYear()+parseInt(years);
         var result = document.getElementById("result").value;
         document.getElementById("result").innerHTML="<br/>" + 
-            "If you deposit $"+principal+",<br/>" + 
-            "at an interest rate of " + 
-            rate +"%.<br/>" + 
-            "You will receive an amount of " 
-            + interest + ",<br/>" + "in the year " + year + "<br/>"
+            "If you deposit $<mark>"+principal+"</mark>,<br/>" + 
+            "at an interest rate of <mark>" + 
+            rate +"%</mark>.<br/>" + 
+            "You will receive an amount of <mark>" 
+            + interest + "</mark>,<br/>" + "in the year <mark>" + year + "</mark><br/>"
     }
 }
 
